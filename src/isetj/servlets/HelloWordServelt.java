ea3201479013bdcd2dbe66aa8d6be725ec4ee01d
Malek <Malek@192.168.1.110>
2021-03-03 22:12:09 +0100

    servelt java

---
+++ src/isetj/servlets/HelloWordServelt.java
@@ -0,0 +1,44 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWordServelt
+ */
+@WebServlet("/HelloWordServelt")
+public class HelloWordServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWordServelt() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out =response.getWriter();
+		out.println("<head><TITLE> HELLO WORLD SERVLET </TITLE></head>");
+		out.println("<font color ='teal'> <h2> hello world !</h2></font>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	doGet(request,response);
+	}
+
+}
